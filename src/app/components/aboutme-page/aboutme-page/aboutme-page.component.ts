cdce2cddfba8fdbad5db213975b49c4edc9293d5
Alina <al.fedorova02@gmail.com>
2022-07-26 15:49:09 +0700

    first commit

---
+++ src/app/components/aboutme-page/aboutme-page/aboutme-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutme-page',
+  templateUrl: './aboutme-page.component.html',
+  styleUrls: ['./aboutme-page.component.css'],
+})
+export class AboutmePageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
